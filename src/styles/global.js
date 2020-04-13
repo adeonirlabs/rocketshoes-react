8cada613ef8d35ad5bdc84efc60edbcd468a17af
Adeonir Kohl <adeonir@gmail.com>
2020-04-13 00:03:42 -0300

    Fix font import

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,8 +3,6 @@ import { createGlobalStyle } from 'styled-components'
 import background from '../assets/images/background.svg'
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
-
   * {
     box-sizing: border-box;
     margin: 0;
