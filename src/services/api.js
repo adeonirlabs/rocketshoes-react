008393fc98a125b5567f722b504fb9909ed9c024
Adeonir Kohl <adeonir@gmail.com>
2020-04-19 17:55:15 -0300

    Update api to use json api from github repository

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
+let baseURL = ''
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3333'
+} else {
+  baseURL = 'https://my-json-server.typicode.com/adeonir/rocketshoes-api'
+}
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL,
 })
 
 export default api
